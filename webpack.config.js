5dc17daecd3282b6eb07e388b6a554b3cc0b7898
OnionPowder01 <autonatione36@gmail.com>
2022-10-31 19:49:06 +0200

    configuration

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,4 @@
+const { NONAME } = require('dns');
 const path = require('path');
 
 module.exports = {
@@ -5,5 +6,6 @@ module.exports = {
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
+    
   },
 };
