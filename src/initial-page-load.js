ee3b35b7117d17a2491d890d067f895d4b51104e
OnionPowder01 <autonatione36@gmail.com>
2022-11-01 22:50:14 +0200

    delete html and make bare-bones in js

---
+++ src/initial-page-load.js
@@ -0,0 +1,28 @@
+export function intialPageLoad() {
+    const contentDiv = document.querySelector("#content");
+
+    /* _________ page-title __________ */
+    
+    const heading = document.createElement("h1");
+    heading.textContent = "Welcome to Hulk smash Restaurant";
+    heading.classList.add("landing-title");
+    contentDiv.appendChild(heading);
+
+    /* _________ first-paragraphs __________ */
+
+    const para1 = document.createElement('p');
+    para1.classList.add('landing-page-copy');
+    para1.textContent = 'Ahh, getting stronger, I can feel it!'
+    contentDiv.appendChild(para1);
+
+    const para2 = document.createElement('p');
+    para2.classList.add('landing-page-copy');
+    para2.textContent = 'This is a restaurant where we allways have time to break stuff'
+    contentDiv.appendChild(para2);
+
+    const para3 = document.createElement('p');
+    para3.classList.add('landing-page-copy');
+    para3.textContent = 'Thanks, Hulk!'
+    contentDiv.appendChild(para3);
+
+}
