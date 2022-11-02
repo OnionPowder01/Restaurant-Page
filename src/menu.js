444449a4a74e12aee6358ac4a5af0fd88a9492e4
OnionPowder01 <autonatione36@gmail.com>
2022-11-02 12:22:02 +0200

    first dish js css

--- src/menu.js
+++ src/menu.js
@@ -1,31 +1,36 @@
+import './style.css';
+import tomahawkImg from './assets/images/TomahawkSteak.jpg';
+
 export function menu() {
 
 const contentDiv = document.querySelector("#content");
+const newDiv = document.createElement("div")
+newDiv.classList.add('container')
 
  //Remove any existing DOM children if present
-contentDiv.replaceChild();
+contentDiv.replaceChildren();
 
   /* _________ page-title __________ */
 
 const heading = document.createElement("h1");
 heading.textContent = "Welcome to Hulk smash Restaurant";
-heading.classList.add("landing-title");
-contentDiv.appendChild(heading);
+heading.classList.add("landing-title-menu");
+newDiv.appendChild(heading);
 
  /* _________DOM for Tomahawk Steak__________ */
 
-const tomahawkSteak = document.createElement("img");
-tomahawkSteak.classList.add("image");
-tomahawkSteak.src = "./assets/TomahawkSteak.jpg";
+const tomahawkSteak = new Image();
+tomahawkSteak.classList.add("image-menu");
+tomahawkSteak.src = tomahawkImg;
 tomahawkSteak.alt = "Image of a Tomahawk Steak";
-contentDiv.appendChild(tomahawkSteak);
+newDiv.appendChild(tomahawkSteak);
 
   /* _________DOM for paragraph for Tomahawk Steak __________ */
 
 const para1Steak = document.createElement("p");
 para1Steak.classList.add("menu-copy");
 para1Steak.textContent = "A huge bone in ribeye that is rich, tender, juicy and full of fantastic avour. This steak has a generous marbling throughout and is perfect for sharing."
-contentDiv.appendChild(para1Steak);
+newDiv.appendChild(para1Steak);
 
 
 
@@ -34,5 +39,5 @@ contentDiv.appendChild(para1Steak);
 
 
 
-
+contentDiv.appendChild(newDiv);
 }
