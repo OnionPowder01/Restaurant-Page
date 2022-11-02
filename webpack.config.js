4767fd651bda2c3121bf799fbdf58552718449a7
OnionPowder01 <autonatione36@gmail.com>
2022-11-02 12:20:39 +0200

    jpg and css webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,25 @@
 const { NONAME } = require('dns');
 const path = require('path');
 
-module.exports = {
+module.exports = 
+{
   entry: './src/index.js',
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
-    watch: true,
-    
   },
+  
+      module: {
+        rules: [
+          {
+            test: /\.css$/i,
+            use: ['style-loader', 'css-loader'],
+          },
+         {
+           test: /\.(png|svg|jpg|jpeg|gif)$/i,
+           type: 'asset/resource',
+         },
+        ],
+      },  
 };
+
